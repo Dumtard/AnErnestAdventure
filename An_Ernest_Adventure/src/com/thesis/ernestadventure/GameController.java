ea013e0a73e2d183f0812757d6f9124380cc214b
Charles Black <charles.black90@gmail.com>
2013-12-04 00:41:28 -0500

    Some shitty collision

--- An_Ernest_Adventure/src/com/thesis/ernestadventure/GameController.java
+++ An_Ernest_Adventure/src/com/thesis/ernestadventure/GameController.java
@@ -11,137 +11,187 @@ import com.thesis.ernestadventure.Network.Stop;
 
 public class GameController {
   private final float GRAVITY = 0.5f;
-  
+
   private HashMap<String, Player> players;
-  
+
   private Client client;
-  
+
   private Area area;
-  
-  public GameController(Client client, HashMap<String, Player> players, Area area) {
+
+  public GameController(Client client, HashMap<String, Player> players,
+      Area area) {
     this.client = client;
     this.players = players;
     this.area = area;
   }
-  
+
+  // public void update(float delta) {
+  // for (Map.Entry<String, Player> player: players.entrySet()) {
+  //
+  // //Apply Gravity
+  // player.getValue().setVelocity(player.getValue().getVelocity().x,
+  // player.getValue().getVelocity().y - GRAVITY);
+  //
+  // int tilePositionX = (int)(player.getValue().getPosition().x / Tile.SIZE);
+  // int tilePositionY = (int)(player.getValue().getPosition().y / Tile.SIZE);
+  //
+  // //Collision
+  // if (tilePositionX >= 0 && tilePositionX < area.width &&
+  // tilePositionY >= 0 && tilePositionY < area.height) {
+  //
+  // player.getValue().setPosition(player.getValue().getPosition().x +
+  // (player.getValue().getVelocity().x * delta * Tile.SIZE),
+  // player.getValue().getPosition().y + (player.getValue().getVelocity().y *
+  // delta * Tile.SIZE));
+  //
+  //
+  // // player.getValue().setPosition(player.getValue().getPosition().x,
+  // // player.getValue().getPosition().y + (player.getValue().getVelocity().y *
+  // delta * Tile.SIZE));
+  //
+  // //Bottom
+  // if (area.tiles[tilePositionX][tilePositionY].collidable) {
+  // player.getValue().setPosition(player.getValue().getPosition().x,
+  // ((tilePositionY+1)*Tile.SIZE));
+  // player.getValue().setVelocity(player.getValue().getVelocity().x, 0);
+  // player.getValue().setIsGrounded(true);
+  //
+  // //Top
+  // } else if (area.tiles[tilePositionX][tilePositionY+2].collidable) {
+  // player.getValue().setVelocity(player.getValue().getVelocity().x, 0);
+  // player.getValue().setPosition(player.getValue().getPosition().x,
+  // (tilePositionY * Tile.SIZE)-1);
+  // }
+  //
+  // player.getValue().setPosition(player.getValue().getPosition().x +
+  // (player.getValue().getVelocity().x * delta * Tile.SIZE),
+  // player.getValue().getPosition().y);
+  //
+  // //Right
+  // // if (area.tiles[tilePositionX+1][tilePositionY].collidable ||
+  // // area.tiles[tilePositionX+1][tilePositionY+1].collidable) {
+  // // player.getValue().setVelocity(0, player.getValue().getVelocity().y);
+  // // player.getValue().setPosition(tilePositionX * Tile.SIZE,
+  // player.getValue().getPosition().y);
+  // //
+  // // //Left
+  // // } else if (area.tiles[tilePositionX][tilePositionY].collidable ||
+  // // area.tiles[tilePositionX][tilePositionY+1].collidable) {
+  // // player.getValue().setVelocity(0, player.getValue().getVelocity().y);
+  // // player.getValue().setPosition(((tilePositionX + 1) * Tile.SIZE)+1,
+  // player.getValue().getPosition().y);
+  // // }
+  //
+  // //Update Player location
+  // // player.getValue().setPosition(player.getValue().getPosition().x +
+  // (player.getValue().getVelocity().x * delta * Tile.SIZE),
+  // // player.getValue().getPosition().y + (player.getValue().getVelocity().y *
+  // delta * Tile.SIZE));
+  //
+  // }
+  // }
+  // }
+
   public void update(float delta) {
-    for (Map.Entry<String, Player> player: players.entrySet()) {
 
-      //Apply Gravity
+    for (Map.Entry<String, Player> player : players.entrySet()) {
+      // Apply Gravity
       player.getValue().setVelocity(player.getValue().getVelocity().x, player.getValue().getVelocity().y - GRAVITY);
-      
-      //Update Player location
-      player.getValue().setPosition(player.getValue().getPosition().x + (player.getValue().getVelocity().x * delta * Tile.SIZE),
-                                    player.getValue().getPosition().y + (player.getValue().getVelocity().y * delta * Tile.SIZE));
-      
-      int tilePositionTLX = (int)(player.getValue().getPosition().x / Tile.SIZE);
-      int tilePositionTLY = (int)((player.getValue().getPosition().y + ((Tile.SIZE * 2) - 1)) / Tile.SIZE);
-      
-      int tilePositionTRX = (int)((player.getValue().getPosition().x + (Tile.SIZE - 1)) / Tile.SIZE);
-      int tilePositionTRY = (int)((player.getValue().getPosition().y + ((Tile.SIZE*2) - 1)) / Tile.SIZE);
-      
-      int tilePositionMLX = (int)(player.getValue().getPosition().x / Tile.SIZE);
-      int tilePositionMLY = (int)((player.getValue().getPosition().y + (Tile.SIZE - 1)) / Tile.SIZE);
-      
-      int tilePositionMRX = (int)((player.getValue().getPosition().x + (Tile.SIZE - 1)) / Tile.SIZE);
-      int tilePositionMRY = (int)((player.getValue().getPosition().y + (Tile.SIZE - 1)) / Tile.SIZE);
-      
-      int tilePositionBLX = (int)(player.getValue().getPosition().x / Tile.SIZE);
-      int tilePositionBLY = (int)(player.getValue().getPosition().y / Tile.SIZE);
-      
-      int tilePositionBRX = (int)((player.getValue().getPosition().x + (Tile.SIZE - 1)) / Tile.SIZE);
-      int tilePositionBRY = (int)(player.getValue().getPosition().y / Tile.SIZE);
 
+      // Collision
 
-      
-      //Collision
-      if (tilePositionTLX >= 0 && tilePositionTRX < area.width &&
-          tilePositionBLY >= 0 && tilePositionTLY < area.height) {
+      int tilePositionX = 0;
+      int tilePositionY = 0;
+
+      if (tilePositionX >= 0 && tilePositionX < area.width && tilePositionY >= 0 && tilePositionY < area.height) {
+
+        tilePositionX = (int) ((player.getValue().getPosition().x + (player.getValue().getVelocity().x * delta * Tile.SIZE)) / Tile.SIZE);
+        tilePositionY = (int) ((player.getValue().getPosition().y) / Tile.SIZE);
         
-        //Bottom
-        if (area.tiles[tilePositionBLX][tilePositionBLY].collidable ||
-            area.tiles[tilePositionBRX][tilePositionBRY].collidable) {
-          player.getValue().setPosition(player.getValue().getPosition().x, (tilePositionMLY * Tile.SIZE));
+        // Update Player location
+        player.getValue().setPosition(player.getValue().getPosition().x + (player.getValue().getVelocity().x * delta * Tile.SIZE),
+                                      player.getValue().getPosition().y);
+
+        // Right
+        if (area.tiles[tilePositionX+1][tilePositionY].collidable ||
+            area.tiles[tilePositionX+1][tilePositionY+1].collidable) {
+          player.getValue().setPosition((tilePositionX * Tile.SIZE)-1, player.getValue().getPosition().y);
+          player.getValue().setVelocity(0, player.getValue().getVelocity().y);
+  
+        // Left
+        } else if (area.tiles[tilePositionX][tilePositionY].collidable ||
+                   area.tiles[tilePositionX][tilePositionY+1].collidable) {
+          player.getValue().setPosition(((tilePositionX+1) * Tile.SIZE)+1, player.getValue().getPosition().y);
+          player.getValue().setVelocity(0, player.getValue().getVelocity().y);
+        }
+
+        tilePositionX = (int) ((player.getValue().getPosition().x) / Tile.SIZE);
+        tilePositionY = (int) ((player.getValue().getPosition().y + (player.getValue().getVelocity().y * delta * Tile.SIZE)) / Tile.SIZE);
+        
+        
+        // Update Player location
+        player.getValue().setPosition(player.getValue().getPosition().x,
+                                    player.getValue().getPosition().y + (player.getValue().getVelocity().y * delta * Tile.SIZE));
+
+        // Bottom
+        if (area.tiles[tilePositionX][tilePositionY].collidable ||
+            area.tiles[tilePositionX+1][tilePositionY].collidable) {
+          player.getValue().setPosition(player.getValue().getPosition().x, (tilePositionY+1) * Tile.SIZE);
           player.getValue().setVelocity(player.getValue().getVelocity().x, 0);
           player.getValue().setIsGrounded(true);
   
-        //Top
-        } else if (area.tiles[tilePositionTLX][tilePositionTLY].collidable ||
-                   area.tiles[tilePositionTRX][tilePositionTRY].collidable) {
-          player.getValue().setPosition(player.getValue().getPosition().x, (tilePositionBLY * Tile.SIZE));
+        // Top
+        } else if (area.tiles[tilePositionX][tilePositionY+2].collidable ||
+                   area.tiles[tilePositionX+1][tilePositionY+2].collidable) {
+          player.getValue().setPosition(player.getValue().getPosition().x, tilePositionY * Tile.SIZE);
           player.getValue().setVelocity(player.getValue().getVelocity().x, 0);
         }
-        
-        tilePositionTLX = (int)(player.getValue().getPosition().x / Tile.SIZE);
-        tilePositionTLY = (int)((player.getValue().getPosition().y + ((Tile.SIZE * 2) - 1)) / Tile.SIZE);
-        
-        tilePositionTRX = (int)((player.getValue().getPosition().x + (Tile.SIZE - 1)) / Tile.SIZE);
-        tilePositionTRY = (int)((player.getValue().getPosition().y + ((Tile.SIZE*2) - 1)) / Tile.SIZE);
-        
-        tilePositionMLX = (int)(player.getValue().getPosition().x / Tile.SIZE);
-        tilePositionMLY = (int)((player.getValue().getPosition().y + (Tile.SIZE - 1)) / Tile.SIZE);
-        
-        tilePositionMRX = (int)((player.getValue().getPosition().x + (Tile.SIZE - 1)) / Tile.SIZE);
-        tilePositionMRY = (int)((player.getValue().getPosition().y + (Tile.SIZE - 1)) / Tile.SIZE);
-        
-        tilePositionBLX = (int)(player.getValue().getPosition().x / Tile.SIZE);
-        tilePositionBLY = (int)(player.getValue().getPosition().y / Tile.SIZE);
-        
-        tilePositionBRX = (int)((player.getValue().getPosition().x + (Tile.SIZE - 1)) / Tile.SIZE);
-        tilePositionBRY = (int)(player.getValue().getPosition().y / Tile.SIZE);
-        
-        //Right   
-        if ((area.tiles[tilePositionTRX][tilePositionTRY].collidable) ||
-            (area.tiles[tilePositionMRX][tilePositionMRY].collidable) ||
-            (area.tiles[tilePositionBRX][tilePositionBRY].collidable)) {
-          player.getValue().setPosition(tilePositionBLX * Tile.SIZE, player.getValue().getPosition().y);
-          player.getValue().setVelocity(0, player.getValue().getVelocity().y);
-        
-        //Left
-        } else if ((area.tiles[tilePositionTLX][tilePositionTLY].collidable) ||
-                   (area.tiles[tilePositionMLX][tilePositionMLY].collidable) ||
-                   (area.tiles[tilePositionBLX][tilePositionBLY].collidable)) {
-          player.getValue().setPosition(tilePositionBRX * Tile.SIZE, player.getValue().getPosition().y);
-          player.getValue().setVelocity(0, player.getValue().getVelocity().y);
-        }
-        
+  
 
       }
     }
   }
-  
+
   public void keyDown(int keycode) {
-    //Left Arrow && Right Arrow
+    // Left Arrow && Right Arrow
     if (Gdx.input.isKeyPressed(21) && Gdx.input.isKeyPressed(22)) {
-      players.get(ErnestGame.loginName).setVelocity(new Vector2(0, players.get(ErnestGame.loginName).getVelocity().y));
+      players.get(ErnestGame.loginName).setVelocity(
+          new Vector2(0, players.get(ErnestGame.loginName).getVelocity().y));
       Stop stop = new Stop();
       stop.name = ErnestGame.loginName;
       stop.position = players.get(ErnestGame.loginName).getPosition();
       client.sendUDP(stop);
-      
-    //Left Arrow
+
+      // Left Arrow
     } else if (Gdx.input.isKeyPressed(21)) {
-      players.get(ErnestGame.loginName).setVelocity(new Vector2(-3.0f, players.get(ErnestGame.loginName).getVelocity().y));
+      players.get(ErnestGame.loginName)
+          .setVelocity(
+              new Vector2(-3.0f, players.get(ErnestGame.loginName)
+                  .getVelocity().y));
       players.get(ErnestGame.loginName).setIsFacingRight(false);
       Move move = new Move();
       move.name = ErnestGame.loginName;
       move.velocity = players.get(ErnestGame.loginName).getVelocity();
       client.sendUDP(move);
-      
-    //Right Arrow
+
+      // Right Arrow
     } else if (Gdx.input.isKeyPressed(22)) {
-      players.get(ErnestGame.loginName).setVelocity(new Vector2(3.0f, players.get(ErnestGame.loginName).getVelocity().y));
+      players.get(ErnestGame.loginName).setVelocity(
+          new Vector2(3.0f, players.get(ErnestGame.loginName).getVelocity().y));
       players.get(ErnestGame.loginName).setIsFacingRight(true);
       Move move = new Move();
       move.name = ErnestGame.loginName;
       move.velocity = players.get(ErnestGame.loginName).getVelocity();
       client.sendUDP(move);
-    }  
-      
-    //Space Bar
+    }
+
+    // Space Bar
     if (keycode == 62) {
       if (players.get(ErnestGame.loginName).getIsGrounded()) {
-        players.get(ErnestGame.loginName).setVelocity(new Vector2(players.get(ErnestGame.loginName).getVelocity().x, 8.5f));
+        players.get(ErnestGame.loginName)
+            .setVelocity(
+                new Vector2(players.get(ErnestGame.loginName).getVelocity().x,
+                    8.5f));
         players.get(ErnestGame.loginName).setIsGrounded(false);
         Move move = new Move();
         move.name = ErnestGame.loginName;
@@ -150,38 +200,45 @@ public class GameController {
       }
     }
   }
-  
+
   public void keyUp(int keycode) {
     // Gdx.app.log("Key", "" + keycode);
 
-    //Left Arrow && Right Arrow
+    // Left Arrow && Right Arrow
     if (Gdx.input.isKeyPressed(21) && Gdx.input.isKeyPressed(22)) {
-      players.get(ErnestGame.loginName).setVelocity(new Vector2(players.get(ErnestGame.loginName).getVelocity().x, players.get(ErnestGame.loginName).getVelocity().y));
-//      Move move = new Move();
-//      move.name = ErnestGame.loginName;
-//      move.velocity = players.get(ErnestGame.loginName).getVelocity();
-//      client.sendUDP(move);
-      
-    //Left Arrow
+      players.get(ErnestGame.loginName).setVelocity(
+          new Vector2(players.get(ErnestGame.loginName).getVelocity().x,
+              players.get(ErnestGame.loginName).getVelocity().y));
+      // Move move = new Move();
+      // move.name = ErnestGame.loginName;
+      // move.velocity = players.get(ErnestGame.loginName).getVelocity();
+      // client.sendUDP(move);
+
+      // Left Arrow
     } else if (Gdx.input.isKeyPressed(21)) {
-      players.get(ErnestGame.loginName).setVelocity(new Vector2(-3.0f, players.get(ErnestGame.loginName).getVelocity().y));
+      players.get(ErnestGame.loginName)
+          .setVelocity(
+              new Vector2(-3.0f, players.get(ErnestGame.loginName)
+                  .getVelocity().y));
       players.get(ErnestGame.loginName).setIsFacingRight(false);
       Move move = new Move();
       move.name = ErnestGame.loginName;
       move.velocity = players.get(ErnestGame.loginName).getVelocity();
       client.sendUDP(move);
-      
-    //Right Arrow
+
+      // Right Arrow
     } else if (Gdx.input.isKeyPressed(22)) {
-      players.get(ErnestGame.loginName).setVelocity(new Vector2(3.0f, players.get(ErnestGame.loginName).getVelocity().y));
+      players.get(ErnestGame.loginName).setVelocity(
+          new Vector2(3.0f, players.get(ErnestGame.loginName).getVelocity().y));
       players.get(ErnestGame.loginName).setIsFacingRight(true);
       Move move = new Move();
       move.name = ErnestGame.loginName;
       move.velocity = players.get(ErnestGame.loginName).getVelocity();
       client.sendUDP(move);
-    
+
     } else {
-      players.get(ErnestGame.loginName).setVelocity(new Vector2(0, players.get(ErnestGame.loginName).getVelocity().y));
+      players.get(ErnestGame.loginName).setVelocity(
+          new Vector2(0, players.get(ErnestGame.loginName).getVelocity().y));
       Stop stop = new Stop();
       stop.name = ErnestGame.loginName;
       stop.position = players.get(ErnestGame.loginName).getPosition();
